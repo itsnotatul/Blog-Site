c7a1715e5817d3ec045ff9c13d8f5715aed443eb
root <atultiwariee60@gmail.com>
2020-04-07 19:54:03 +0000

    make all three routes

---
+++ routes/blogs.js
@@ -0,0 +1,93 @@
+var express = require("express");
+var router  = express.Router();
+var Blog = require("../models/blog");
+var mongoose   = require("mongoose");
+var middleware = require("../middleware");
+
+//INDEX - show all blogs
+router.get("/blogs",function(req,res){
+		Blog.find({},function(err,blogs){
+			if(err){
+				console.log(err);
+			}else{
+				res.render("blogs/index",{blogs:blogs});
+			}
+		});
+});
+
+//NEW route
+router.get("/blogs/new",middleware.isLoggedIn,function(req,res){
+	res.render("blogs/new");
+})
+
+//CREATE Route
+router.post("/blogs",function(req,res){
+	//sanitize the body(content of blog) coming from the form under blog so that no //script tags are used 
+	req.body.blog.body=req.sanitize(req.body.blog.body);
+	
+	//create blog
+	Blog.create(req.body.blog,function(err,newBlog){ // we stored body,img,tit in blog //systematically , so req.body.blogs automaticaly gets all three.
+		if(err){
+			res.render("new");
+		}else{
+			//redirect backto index
+			res.redirect("/blogs");
+		}
+	});
+});
+
+//SHOW Route
+router.get("/blogs/:id",function(req,res){
+var id = mongoose.Types.ObjectId(req.params.id); 
+	Blog.findById(id).populate("comments").exec(function(err,foundBlog){
+		if(err || !foundBlog){
+        req.flash("error","Blog not found");
+			res.redirect("back");
+		}else{
+			res.render("blogs/show",{blog:foundBlog});
+		}
+	});
+});
+
+//EDIT Route
+router.get("/blogs/:id/edit",function(req,res){
+	var id = mongoose.Types.ObjectId(req.params.id);
+	Blog.findById(id,function(err,foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.render("blogs/edit",{blog:foundBlog});
+		}
+	});
+});
+
+//UPDATE Route
+router.put("/blogs/:id",middleware.checkBlogOwnership,function(req,res){
+	req.body.blog.body=req.sanitize(req.body.blog.body);
+	var id= mongoose.Types.ObjectId(req.params.id);
+	Blog.findByIdAndUpdate(id,req.body.blog,function(err,updatedForm){
+		if(err){
+        	req.flash("error", err.message);
+			res.redirect("/blogs");
+		}else{
+           req.flash("success","Successfully Updated!");
+			res.redirect("/blogs/"+id);
+		}
+	});
+	
+});
+
+//DELET Route
+router.delete("/blogs/:id",middleware.checkBlogOwnership,function(req,res){
+	//destroy blog
+	var id = mongoose.Types.ObjectId(req.params.id);
+	Blog.findByIdAndRemove(id,function(err){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.redirect("/blogs");
+		}
+	});
+});
+
+module.exports= router;
---
+++ routes/comments.js
@@ -0,0 +1,107 @@
+var express = require("express");
+var router  = express.Router();
+var mongoose = require("mongoose");
+
+var Blog = require("../models/campground");
+var Comment = require("../models/comment");
+var middleware = require("../middleware");
+
+// COMMENTS ROUTES
+
+router.get("/blogs/:id/comments/new",middleware.isLoggedIn,function(req,res){
+	//find campground by id
+	var id =  mongoose.Types.ObjectId(req.params.id);
+	Blog.findById(id,function(err,blog){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("comments/new",{blog:blog});
+		}
+	}) 
+	
+})
+
+router.post("/blogs/:id/comments",middleware.isLoggedIn,function(req,res){
+	// find campground by id
+	var id =  mongoose.Types.ObjectId(req.params.id);
+	Blog.findById(id,function(err,blog){
+		if(err){
+			console.log(err);
+			res.redirect("/blogs");
+		}else{
+			Comment.create(req.body.comment,function(err,comment){
+				if(err){
+					req.flash("error","Something went wrong")
+					console.log(err);
+				}else{
+//we'll get here inly if its logged in so, req.user contain our user					
+					//add username n id to comment
+					comment["author"]["id"]=req.user._id;
+					comment["author"]["username"]=req.user.username;
+					//save comment
+					comment.save();
+					blog.comments.push(comment);
+					blog.save();
+					req.flash("success","Successfully added comment");
+					res.redirect("/blogs/"+id);
+				}
+			});
+			
+		}
+	})
+});
+
+//edit route
+router.get("/blogs/:id/comments/:comment_id/edit",middleware.checkCommentOwnership,function(req,res){
+	
+	var blog_id =  mongoose.Types.ObjectId(req.params.id);
+	var id =  mongoose.Types.ObjectId(req.params.comment_id);
+	
+// someone can change the camp id in the url and broke our app so 
+	Blog.findById(blog_id,function(err,foundBlog){
+		if(err || !foundBlog){
+			req.flash("error","Blog not found");
+			return res.redirect("back");
+		}
+		//error handling is done now
+		
+		Comment.findById(id,function(err,foundComment){
+		if(err){
+			res.redirect("back");
+		}else{
+	res.render("comments/edit",{blog_id:blog_id,comment:foundComment});
+		}
+	})
+});
+	});
+	
+	
+//Comment UPDATE Route
+router.put("/blogs/:id/comments/:comment_id",middleware.checkCommentOwnership,function(req,res){
+	var id =  mongoose.Types.ObjectId(req.params.comment_id);
+	var id2 =  mongoose.Types.ObjectId(req.params.id);
+	Comment.findByIdAndUpdate(id,req.body.comment,function(err,updatedComment){	
+	if(err){
+		res.redirect("back");
+	}else{
+		res.redirect("/blogs/"+id2);
+	}
+	     })
+	
+});
+//DELETE COMMENT Route
+router.delete("/blogs/:id/comments/:comment_id",middleware.checkCommentOwnership,function(req,res){
+	var id =  mongoose.Types.ObjectId(req.params.comment_id);
+	var id2 =  mongoose.Types.ObjectId(req.params.id);
+	
+	 Comment.findByIdAndRemove(id, function(err){
+       if(err){
+           res.redirect("back");
+       } else {
+           req.flash("success", "Comment deleted");
+           res.redirect("/blogs/" + id2);
+       }
+    });
+});
+
+module.exports= router;
